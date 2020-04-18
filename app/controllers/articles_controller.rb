e6a7ba6dc471e50b497a69b90c193f2ce20585af
sirokumasan <fsho2262@gmail.com>
2020-04-18 14:04:48 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,7 +56,6 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
     end
